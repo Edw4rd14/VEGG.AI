6913f03f1ee4cdeb16010a9b316693d9101fa99f
Edward <2b04.2214407.edwardtan@gmail.com>
2024-02-11 15:39:06 +0000

    Updated code

--- tests/test_api.py
+++ tests/test_api.py
@@ -23,25 +23,19 @@ class TestRESTful:
     # - The table constraints allow valid values from being added
     @classmethod
     @pytest.mark.parametrize('entry_list',[
-        ['0001.jpg', './prediction-testing-images/0001.jpg', 128, 'Cabbage'],
-        ['0001.jpg', './prediction-testing-images/0001.jpg', 31, 'Cabbage'],
-        ['0022.jpg', './prediction-testing-images/0022.jpg', 128, 'Brinjal'],
-        ['0142.png', './prediction-testing-images/0142.jpg', 31, 'Tomato'],
-        ['0034.jpg', './prediction-testing-images/0034.jpg', 128, 'Bean'],
-        ['0052.jpg', './prediction-testing-images/0052.jpg', 31, 'Potato'],
-        ['0022.png', './prediction-testing-images/0022.jpg', 31, 'Brinjal'],
-        ['0142.jpg', './prediction-testing-images/0142.jpg', 128, 'Tomato'],
-        ['0034.jpg', './prediction-testing-images/0034.jpg', 31, 'Bean'],
-        ['0052.png', './prediction-testing-images/0052.jpg', 128, 'Potato']
+        {'file_name': '0001.jpg', 'image': image_to_b64('./prediction-testing-images/0001.jpg'), 'image_size': 128, 'prediction': 'Cabbage'},
+        {'file_name': '0001.jpg', 'image': image_to_b64('./prediction-testing-images/0001.jpg'), 'image_size': 31, 'prediction': 'Cabbage'},
+        {'file_name': '0022.jpg', 'image': image_to_b64('./prediction-testing-images/0022.jpg'), 'image_size': 128, 'prediction': 'Brinjal'},
+        {'file_name': '0142.png', 'image': image_to_b64('./prediction-testing-images/0142.jpg'), 'image_size': 31, 'prediction': 'Tomato'},
+        {'file_name': '0034.jpg', 'image': image_to_b64('./prediction-testing-images/0034.jpg'), 'image_size': 128, 'prediction': 'Bean'},
+        {'file_name': '0052.jpg', 'image': image_to_b64('./prediction-testing-images/0052.jpg'), 'image_size': 31, 'prediction': 'Potato'},
+        {'file_name': '0022.png', 'image': image_to_b64('./prediction-testing-images/0022.jpg'), 'image_size': 31, 'prediction': 'Brinjal'},
+        {'file_name': '0142.jpg', 'image': image_to_b64('./prediction-testing-images/0142.jpg'), 'image_size': 128, 'prediction': 'Tomato'},
+        {'file_name': '0034.jpg', 'image': image_to_b64('./prediction-testing-images/0034.jpg'), 'image_size': 31, 'prediction': 'Bean'},
+        {'file_name': '0052.png', 'image': image_to_b64('./prediction-testing-images/0052.jpg'), 'image_size': 128, 'prediction': 'Potato'}
     ])
     def test_add_api(self,client,entry_list):
-        data = { 
-            'file_name': entry_list[0],
-            'image': image_to_b64(entry_list[1]),
-            'image_size': entry_list[2], 
-            'prediction': entry_list[3],
-        }
-        response = client.post('/api/add',data=json.dumps(data),content_type="application/json", follow_redirects=True)
+        response = client.post('/api/add',data=json.dumps(entry_list),content_type="application/json", follow_redirects=True)
         # Assert statements
         assert response.status_code == 200
         assert response.headers["Content-Type"] == "application/json"
@@ -55,20 +49,14 @@ class TestRESTful:
     @classmethod
     @pytest.mark.xfail(reason='Incorrect file name, image, image size or prediction')
     @pytest.mark.parametrize('entry_list',[
-        ['0001', './prediction-testing-images/0001.jpg', 31, 'Pumpkin'],
-        ['0001.png', './prediction-testing-images/0001.jpg', -100, 'Pumpkin'],
-        ['0001.webp', './prediction-testing-images/0001.jpg', 31, 'Pumpkin'],
-        ['0001.jpg', './prediction-testing-images/0001.jpg', 128, 'Mcspicy'],
-        ['0001.jpg', '', 128, 'Cucumber'],
+        {'file_name': '0001', 'image': image_to_b64('./prediction-testing-images/0001.jpg'), 'image_size': 31, 'prediction': 'Pumpkin'},
+        {'file_name': '0001.png', 'image': image_to_b64('./prediction-testing-images/0001.jpg'), 'image_size': -100, 'prediction': 'Pumpkin'},
+        {'file_name': '0001.webp', 'image': image_to_b64('./prediction-testing-images/0001.jpg'), 'image_size': 31, 'prediction': 'Pumpkin'},
+        {'file_name': '0001.jpg', 'image': image_to_b64('./prediction-testing-images/0001.jpg'), 'image_size': 128, 'prediction': 'Mcspicy'},
+        {'file_name': '0001.jpg', 'image': '', 'image_size': 128, 'prediction': 'Cucumber'},
     ])
     def test_add_api_incorrect(self,client,entry_list):
-        data = { 
-            'file_name': entry_list[0],
-            'image': image_to_b64(entry_list[1]),
-            'image_size': entry_list[2], 
-            'prediction': entry_list[3],
-        }
-        response = client.post('/api/add',data=json.dumps(data),content_type="application/json", follow_redirects=True)
+        response = client.post('/api/add',data=json.dumps(entry_list),content_type="application/json", follow_redirects=True)
         # Assert statements
         response_body = json.loads(response.get_data(as_text=True))
         # These should also FAIL as the table constraints should prevent the values from being added, hence these checks should return FALSE for xfail flag to be correctly shown
@@ -124,27 +112,23 @@ class TestRESTful:
     @staticmethod
     @pytest.mark.filterwarnings('ignore')
     @pytest.mark.parametrize('entry_list',[
-        ['./prediction-testing-images/0001.jpg', 128, 'Cabbage'],
-        ['./prediction-testing-images/0001.jpg', 31, 'Cabbage'],
-        ['./prediction-testing-images/0022.jpg', 128, 'Brinjal'],
-        ['./prediction-testing-images/0142.jpg', 31, 'Tomato'],
-        ['./prediction-testing-images/0034.jpg', 128, 'Bean'],
-        ['./prediction-testing-images/0052.jpg', 31, 'Potato'],
-        ['./prediction-testing-images/0022.jpg', 31, 'Brinjal'],
-        ['./prediction-testing-images/0142.jpg', 128, 'Tomato'],
-        ['./prediction-testing-images/0034.jpg', 31, 'Bean'],
-        ['./prediction-testing-images/0052.jpg', 128, 'Potato']
+        {'image': image_to_b64('./prediction-testing-images/0001.jpg'), 'image_size': 128, 'prediction': 'Cabbage'},
+        {'image': image_to_b64('./prediction-testing-images/0001.jpg'), 'image_size': 31, 'prediction': 'Cabbage'},
+        {'image': image_to_b64('./prediction-testing-images/0022.jpg'), 'image_size': 128, 'prediction': 'Brinjal'},
+        {'image': image_to_b64('./prediction-testing-images/0142.jpg'), 'image_size': 31, 'prediction': 'Tomato'},
+        {'image': image_to_b64('./prediction-testing-images/0034.jpg'), 'image_size': 128, 'prediction': 'Bean'},
+        {'image': image_to_b64('./prediction-testing-images/0052.jpg'), 'image_size': 31, 'prediction': 'Potato'},
+        {'image': image_to_b64('./prediction-testing-images/0022.jpg'), 'image_size': 31, 'prediction': 'Brinjal'},
+        {'image': image_to_b64('./prediction-testing-images/0142.jpg'), 'image_size': 128, 'prediction': 'Tomato'},
+        {'image': image_to_b64('./prediction-testing-images/0034.jpg'), 'image_size': 31, 'prediction': 'Bean'},
+        {'image': image_to_b64('./prediction-testing-images/0052.jpg'), 'image_size': 128, 'prediction': 'Potato'}
     ])
     def test_prediction(client, entry_list):
-        data = { 
-            'image': image_to_b64(entry_list[0]),
-            'image_size': entry_list[1], 
-        }
         response = client.post('/api/predict', 
-                               data=json.dumps(data),
+                               data=json.dumps(entry_list),
                                content_type="application/json", 
                                follow_redirects=True
                                )
         assert response.status_code == 200
         assert response.headers["Content-Type"] == "application/json"
-        assert json.loads(response.get_data(as_text=True))['prediction'] == entry_list[2]
+        assert json.loads(response.get_data(as_text=True))['prediction'] == entry_list['prediction']
